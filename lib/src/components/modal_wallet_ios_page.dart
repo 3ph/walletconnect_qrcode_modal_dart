b29c3651e4e9f194baabc7eb57ed7af135ccf714
r9bt <r9bt0702@gmail.com>
2022-08-02 14:21:58 +0900

    特定のウォレットアプリのみ有効に変更（iOS対応）

--- lib/src/components/modal_wallet_ios_page.dart
+++ lib/src/components/modal_wallet_ios_page.dart
@@ -44,58 +44,77 @@ class ModalWalletIOSPage extends StatelessWidget {
                     itemCount: walletData.data!.length,
                     itemBuilder: (context, index) {
                       final wallet = walletData.data![index];
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: GestureDetector(
-                          onTap: () async {
-                            walletCallback?.call(wallet);
-                            Utils.iosLaunch(wallet: wallet, uri: uri);
-                          },
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 16),
-                                  child: Text(
-                                    wallet.name,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
+                      return Stack(children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: GestureDetector(
+                            onTap: () async {
+                              if (shouldEnable(wallet)) {
+                                walletCallback?.call(wallet);
+                                Utils.iosLaunch(wallet: wallet, uri: uri);
+                              }
+                            },
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 16),
+                                    child: Text(
+                                      wallet.name,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                clipBehavior: Clip.hardEdge,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      blurRadius: 5,
-                                      spreadRadius: 2,
-                                    ),
-                                  ],
+                                Container(
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.3),
+                                        blurRadius: 5,
+                                        spreadRadius: 2,
+                                      ),
+                                    ],
+                                  ),
+                                  child: CachedNetworkImage(
+                                    imageUrl:
+                                        'https://registry.walletconnect.org/logo/sm/${wallet.id}.jpeg',
+                                    height: 30,
+                                  ),
                                 ),
-                                child: CachedNetworkImage(
-                                  imageUrl:
-                                      'https://registry.walletconnect.org/logo/sm/${wallet.id}.jpeg',
-                                  height: 30,
+                                const Padding(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child: Icon(
+                                    Icons.arrow_forward_ios,
+                                    size: 20,
+                                    color: Colors.grey,
+                                  ),
                                 ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(left: 8),
-                                child: Icon(
-                                  Icons.arrow_forward_ios,
-                                  size: 20,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      );
+                        if (!shouldEnable(wallet))
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            color: Colors.black.withOpacity(0.8),
+                            child: const Center(
+                              child: Text(
+                                'Coming soon...',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          )
+                      ]);
                     }),
               ),
             ],
@@ -122,11 +141,17 @@ class ModalWalletIOSPage extends StatelessWidget {
         final filter = <Wallet>[];
         for (final wallet in wallets) {
           if (await shouldShow(wallet)) {
-            filter.add(wallet);
+            if (shouldEnable(wallet)) {
+              filter.insert(0, wallet);
+            } else {
+              filter.add(wallet);
+            }
           }
         }
         return filter;
       },
     );
   }
+
+  bool shouldEnable(Wallet wallet) => wallet.name == 'MetaMask';
 }
